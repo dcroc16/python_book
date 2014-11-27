35a896878d04c7d1f0539e180d94e8b95316260b
dcroc16 <davidwcrocker@gmail.com>
2014-11-26 18:03:24 -0600

    working through chapter 3

--- python_book.py
+++ python_book.py
@@ -7,8 +7,11 @@ def pick_a_program():
             '2.2 Favorite Food Mashup',
             '2.3 Tipper App',
             '2.4 Car Salesman',
+            '3.1 fortune cookie',
+            '3.2 flip a coin',
+            '3.3 Guess my Number',
+            '3.4 Computer Guess my Number',
             '[EXIT] End Program'
-
 
         ]
     print "\n\n\n"
@@ -66,8 +69,77 @@ def tipper_app():
         print "Tip @ 20% " + str(base_bill * 0.20) + " : Total Bill " + str(base_bill + (base_bill * 0.20)
 )        
 def car_sales_invoice():
+    try:
+        car_base_price = input("Enter the Car's base prices.................. ")
+    except:
+        print "Make sure you enter a valid number as the car price"
+        car_sales_invoice()
+    else:
+        tax = car_base_price * 0.15
+        license = car_base_price * 0.20
+        dealer_prep = 10000
+        destination_charge = 20.38
+       
+        total = car_base_price + tax + license + dealer_prep + destination_charge
+       
+        print """
+                     INVOICE
+              ---------------------
+             
+              Car Base Price..... %s
+              tax................ %s
+              license............ %s
+              dealer prep........ %s
+              destination charge. %s
+     
+              ---------------------
+              Total Car Price.... %s""" % (car_base_price, tax, license, dealer_prep, destination_charge, total)
+
+
+def fortune_cookie():
+    fortunes = [
+        'fortune 1',
+        'fortune 2',
+        'fortune 3',
+        'fortune 4',
+    ]
+    import random
+    choice = random.randint(0,len(fortunes)-1)
+    print fortunes[choice]
+
+def flip_a_coin():
+    heads = 0
+    tails = 0
+    class Coin:
+        def __init__(self):
+           self.side = "heads"
+        def flip(self):
+            import random
+            result = random.randint(0,1)
+            if result:
+               self.side = "heads"
+               return self.side
+            else:
+               self.side = "tails"
+               return self.side
+    c = Coin()
+    for i in range(50):
+        print "Fliped Coin Result: " +  c.flip()
+        if c.side == "heads":
+            heads += 1
+        else:
+            tails += 1
+
+    print "flip results: Heads %s | Tails %s" % (str(heads), str(tails))
+
+def guess_my_number():
+    guess = []
+    for i in range(10):
+        choice = raw_input("pick a number between 1 and 100: ")
+    
+
+def computer_guess_my_number():
     pass
-        
 
 def choice_router(program_id):
     programs = {
@@ -77,7 +149,10 @@ def choice_router(program_id):
         '2.2' : fav_food,
         '2.3' : tipper_app,
         '2.4' : car_sales_invoice,
-
+        '3.1' : fortune_cookie,
+        '3.2' : flip_a_coin,
+        '3.3' : guess_my_number,
+        '3.4' : computer_guess_my_number
         }
     if program_id in programs:
         programs[program_id]()
